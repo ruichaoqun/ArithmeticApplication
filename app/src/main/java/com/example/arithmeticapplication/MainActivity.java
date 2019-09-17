bd202ca2b288c201fef119832367e135e0f4246a
Administrator <ruichaoqun121@163.com>
2019-09-17 14:07:33 +0800

    70.71提交

--- app/src/main/java/com/example/arithmeticapplication/MainActivity.java
+++ app/src/main/java/com/example/arithmeticapplication/MainActivity.java
@@ -2,6 +2,16 @@ package com.example.arithmeticapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
