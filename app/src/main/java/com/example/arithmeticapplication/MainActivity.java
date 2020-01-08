6593285ced81f5da7a02950666e94a07470164b1
Administrator <ruichaoqun121@163.com>
2020-01-08 20:07:26 +0800

    139动态规划提交

--- app/src/main/java/com/example/arithmeticapplication/MainActivity.java
+++ app/src/main/java/com/example/arithmeticapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.arithmeticapplication;
 
+import android.graphics.Typeface;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
@@ -14,12 +18,19 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mTextView = findViewById(R.id.text);
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Typeface typeface = ResourcesCompat.getFont(MainActivity.this, R.font.simsun);
+                mTextView.setTypeface(typeface);
+            }
+        });
     }
 
     @Override
